3deb5f3101b46e537eea0c7424a8a1e7fd818473
suhyeji <65303837+suhyeji@users.noreply.github.com>
2022-07-07 12:06:12 +0900

    Update doc2.js

--- sourceDir/src/src2/doc2.js
+++ sourceDir/src/src2/doc2.js
@@ -1,5 +1,5 @@
 /**
- * @i18n{ko:두 숫자를 더한다., en:Add two numbers}
+ * $i18n{ko:두 숫자를 더한다., en:Add two numbers}
  * @param {number} a - $i18n{ko:첫 번째 숫자,en:The first number}
  * @param {number} b - $i18n{ko:두 번째 숫자,en:The second number}
  * @returns {number}
